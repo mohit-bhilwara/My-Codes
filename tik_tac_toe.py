15fa1e43e116f5700c01378f74a1717719b851bd
Mohit Bhilwara <105989920+mohit-bhilwara@users.noreply.github.com>
2022-05-26 19:17:41 +0530

    Add files via upload
    tik tac toe game with conditional formatting, function, and while loop.

---
+++ tik_tac_toe.py
@@ -0,0 +1,95 @@
+
+a = 'a'
+b = 'b'
+c = 'c'
+d = 'd' 
+e = 'e'
+f,g,h,i = 'f','g','h','i'
+
+
+def Board():
+    print('',a,"|"'',b,'|''',c )
+    print('---|---|---')
+    print('',d,"|"'',e,'|''',f )
+    print('---|---|---')
+    print('',g,"|"'',h,'|''',i )
+
+
+total_turn = 10
+def chechwin():
+    wins = [[a,b,c],[d,e,f],[g,h,i],[a,d,g,],[b,e,h],[c,f,i],[a,e,i],[g,e,c]]
+
+    for win in wins :
+        if win[0]=="X" and win[1]=="X" and win[2] =="X":
+             Board()
+             print('X won the match')
+             return 1
+          
+        elif win[0]=="O" and win[1]=="O" and win[2] =="O":
+             Board()
+             print('O won the match')
+             return 0
+    
+    
+print('welcom to tik tac teo game')
+turn = 1
+while total_turn > 0:
+    Board()
+    if total_turn >1:
+        if turn == 1 :
+            x= input("X's turn enter any alphabatical position: ")
+            
+            if x == "a":
+                 a= "X"
+            elif x == "b":
+                 b= "X"
+            elif x == "c":
+                 c= "X"
+            elif x == "d":
+                 d= "X"
+            elif x == "e":
+                 e= "X"
+            elif x == "f":
+                 f= "X"
+            elif x == "g":
+                 g= "X"
+            elif x == "h":
+                 h= "X"
+            elif x == "i":
+                 i= "X"
+
+        elif turn == 0:
+            o = input("O's turn enter any alphabatical position: ") 
+     
+            if o == "a":
+                 a= "O"
+            elif o == "b":
+                 b= "O"
+            elif o == "c":
+                 c= "O"
+            elif o == "d":
+                 d= "O"
+            elif o == "e":
+                 e= "O"
+            elif o == "f":
+                 f= "O"
+            elif o == "g":
+                 g= "O"
+            elif o == "h":
+                 h= "O"
+            elif o == "i":
+                 i= "O"
+
+
+    if chechwin() == 1 or chechwin() == 0:
+     
+         break
+#     if chechwin !=1 and chechwin()!=0:
+#           print('match tie')
+
+         
+
+    total_turn = total_turn-1
+    turn = 1 - turn
+        
+print('Gameover')  
